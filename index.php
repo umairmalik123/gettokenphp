60c81cf21f47108add394166d3bc1d39554eb475
umairmalik123 <manaluphor@throwam.com>
2017-04-24 11:39:36 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,61 +1,42 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Semal Shah Tokens Site</title>
+  <title>Get Token Full Versions</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   <div class="container">
-  <center><h1>&#9826; SEMAL SHAH And Pythons Bot Team Token Site &#9826;</h1>
+  <center><h1>&#9826; GET TOKEN FULL VERSION &#9826;</h1>
 </head>
 <div class="panel-heading"></div>
 <body>
  <div id="content">
 <br><br>
 <center>
-<div class="SEMALSHAH">
-<a href="https://www.Facebook.com/offical.shah1" alt="SEMALSHAH" target="_blank">
-<img
-src="https://graph.facebook.com/100010916801394/picture?type=large" alt="Designer_&amp;_Developer" style="border-radius: 99em; border: 2px; box-shadow: 0px 0px 9px 7px rgb(204, 204, 204); padding: 0px;" width="200" height="200"></a><a href="https://www.Facebook.com/offical.shah1" alt="SEMALSHAH" target="_blank">
-<img
-src="https://graph.facebook.com/100010916801394/picture?type=large" alt="Designer_&amp;_Developer" style="border-radius: 99em; border: 2px; box-shadow: 0px 0px 9px 7px rgb(204, 204, 204); padding: 0px;" width="200" height="200"></a>
-<div class="container">
-<marquee behavior="scroll" direction="right" scrollamount="8" scrolldelay="1"><strong><font style="text-shadow: 1px 1px black; color:;" size="4">SEMAL SHAH THE BOT MASTER</font></strong></center></marquee>
-  <center><font color="Red" size="+1"><marquee bgcolor="" direction="left" 
-loop="infinite" width="60%"><strong>DESIGNED BY :- SEMAL SHAH
-</strong></marquee></font></center></div></div></font>
-  
+
   <div class="panel-group">
 <div class="panel panel-primary">
-    <div class="panel-heading">( Type your e-mail and password to get the access token )</div>
+    <div class="panel-heading">GET TOKEN No Checkpoint 100%</div>
       <div class="panel-body">      
 <div class="form-group">
-<label for="usr"> Email </label>
+<label for="usr"> Username: </label>
   <input type="text" class="form-control" id="tk">
 </div>
 <div class="form-group">
-<label for="pwd"> Password </label>
+<label for="pwd"> Password: </label>
   <input type="password" class="form-control" id="mk">
 </div>
 <button type="button" class="btn btn-danger" onclick="Puaru_Active()" >Get Token</button>
 <p>
-<li id="trave" class="list-group-item"><img src="https://graph.facebook.com/100010916801394/picture"> </li></p>
-        Add Semal Shah On Facebook : <a href="https://Www.Facebook.com/offical.shah1" 
-target="blank">Semal Shah</a><br>
-Download Script : <a href="https://Www.Facebook.com/offical.shah1" 
-target="blank">Semal Shah</a>
-       <Br>
-     <Br>   
-<a href="https://www.Facebook.com/offical.shah1" target="_blank" style="font-family: Geneva, Arial; font-size: 9px; color: #330010; text-decoration: none;">
-         </script>
   <br>
 <div id="footer">
 <script language="JavaScript1.2">
 
+<li id="trave" class="list-group-item"><img src="http://i.imgur.com/4xwpZzp.png"> </li></p>
 
-var message="DESIGNED BY SEMAL SHAH"
+var message="DESIGNED BY :- X-Hackerx Team"
 var neonbasecolor=""
 var neontextcolor="RED"
 var flashspeed=100  //in milliseconds
